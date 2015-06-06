39f705f566b67822497a1afd6d2013e75caae397
Basicben <benari_kutai@yahoo.com>
2015-06-06 16:54:27 +0300

    db connection

--- webservices/dbconnection.js
+++ webservices/dbconnection.js
@@ -3,10 +3,13 @@ mongoose.connect();
 
 var conn = mongoose.connection;
 
+// On connection error
 conn.on('error',function(err){
 	console.log('connection error + ' err);
 });
 
+// om connection success
 conn.once('open',function(){
-	console.log('connected.';
+	console.log('connected.');
+	//mongoose.disconnect();
 });
