5050c8568521be8eaf5a04d1b4b904a031abe632
Noam Rom <rom.noam89@gmail.com>
2015-06-08 20:18:57 +0300

    addUser function

--- server.js
+++ server.js
@@ -2,11 +2,8 @@ var express = require('express');
 var app = express();
 
 app.use('/',express.static('./public')).listen(process.env.PORT || 3000);
-console.log('listeing...');
+console.log('listeing on server...');
 
-var insert = require('./webservices/Database/users/db.user').userInsert;
-app.post('/userInsert',function(req,res){
-    console.log(req);
-    console.log(res);
-});
+var addUser = require('./webservices/Database/users/db.user').addUser;
+addUser();
 
--- webservices/Database/users/db.user.js
+++ webservices/Database/users/db.user.js
@@ -1,43 +1,44 @@
 /********       Connecting to database + Creating user schema            **************/
-var mongoose = require('mongoose');
-mongoose.connect("mongodb://benari:123456@ds043972.mongolab.com:43972/db_suitemybeer");
-var userSchema = require('./db.user.schema.js').userSchema;
-var user = mongoose.model('UserM',userSchema);
-var conn = mongoose.connection;
+
 /** **********************************************************/
+var addUser = function() {
+    var mongoose = require('mongoose');
+    mongoose.connect("mongodb://benari:123456@ds043972.mongolab.com:43972/db_suitemybeer");
+    var userSchema = require('./db.user.schema').userSchema;
+    var user = mongoose.model('UserM', userSchema);
+    var conn = mongoose.connection;
 
-var userInsert = function(){
-    console.log("insert....");
+    conn.on('error', function (err) {
+        console.log('Connection to the database as failed ' + err);
+    });
 
+    conn.once('open', function () {
+        console.log("Connected to db_suitemybeer........\n");
+
+        /**********       Adding new user from facebook to User's collection              **********/
+        var newUser = new user({
+
+            FirstName: "FacebookFirstName",
+            LastName: "FacebookLastName",
+            Email: "FacebookUserEmail",
+            MediumProfilePic: "FacebookMediumProfilePic",
+            SmallProfilePic: "FacebookSmallProfilePic",
+            HomeTown: "FacebookUserHometown"
+        });
+
+        /**     Check if user already exist        **/
+        if (newUser.isNew) {
+            newUser.save(function (err, doc) {
+                console.log("\nUser was added to User collection " + doc);
+            })
+        }
+    });
+
+    mongoose.disconnect();
 };
-exports.userInsert = userInsert;
+
+exports.addUser = addUser;
 
 /*****      Connection to the database ( db_suitemybeer ) *****/
 
-conn.on('error',function(err){
-    console.log('connection error' + err);
-});
 
-conn.once('open',function(){
-    console.log("Connected to db_suitemybeer\n");
-
-    /**********       Adding new user from facebook to User's collection              **********/
-    var newUser = new user({
-
-        FirstName: "FacebookFirstName",
-        LastName: "FacebookLastName",
-        Email: "FacebookUserEmail",
-        MediumProfilePic: "FacebookMediumProfilePic",
-        SmallProfilePic: "FacebookSmallProfilePic",
-        HomeTown: "FacebookUserHometown"
-    });
-
-    /**     Check if user already exist        **/
-    if(newUser.isNew) {
-        newUser.save(function (err, doc) {
-            console.log("\nUser was added to User collection " + doc);
-        })
-    }
-
-
-});
