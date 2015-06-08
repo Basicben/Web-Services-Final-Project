5682acbc2ae328d505dcb576c723e3fee84e5f9c
ONB_Benari <ONB_Benari@hotmail.com>
2015-06-08 20:08:27 +0300

    test3

--- public/js/master-app.js
+++ public/js/master-app.js
@@ -32,7 +32,7 @@ suiteApp.controller('masterCntrl', function($scope,$http) {
         $scope.connectedUser = facebookLogin(function(){
             $scope.connectedUser = USER;
             console.log('$scope.connectedUser',$scope.connectedUser);
-            $http.post('http://localhost:3000/userInsert',{user:$scope.connectedUser}).
+            $http.post('https://localhost:3000/userInsert',{user:$scope.connectedUser}).
               success(function(data, status, headers, config) {
                 // this callback will be called asynchronously
                 // when the response is available
