5c56672d01e46f5291ade534039b15afbf5daf28
ONB_Benari <ONB_Benari@hotmail.com>
2015-06-08 19:58:05 +0300

    test1

--- public/js/master-app.js
+++ public/js/master-app.js
@@ -32,6 +32,17 @@ suiteApp.controller('masterCntrl', function($scope) {
         $scope.connectedUser = facebookLogin(function(){
             $scope.connectedUser = USER;
             console.log('$scope.connectedUser',$scope.connectedUser);
+            $http.post('http://localhost:3000/userInsert',{user:$scope.connectedUser}).
+              success(function(data, status, headers, config) {
+                // this callback will be called asynchronously
+                // when the response is available
+                console.log('Success : data',data);
+              }).
+              error(function(data, status, headers, config) {
+                // called asynchronously if an error occurs
+                // or server returns response with an error status.
+                console.log('Error : status',status,'headers',headers);
+              });
         });
     }
 
