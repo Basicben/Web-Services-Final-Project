91ec92d6b5563970e8d09169c26cc893d5ca27ed
Noam Rom <rom.noam89@gmail.com>
2015-06-08 14:04:45 +0300

    datbase connection + schema

--- webservices/Database/dbconnection.js
+++ webservices/Database/dbconnection.js
@@ -1,7 +1,21 @@
+/**      Connecting to the database && creating a new document (usersSchema)       **/
 var mongoose = require('mongoose');
 mongoose.connect("mongodb://benari:123456@ds043972.mongolab.com:43972/db_suitemybeer");
-
 var conn = mongoose.connection;
+
+mongoose.model('UserM',usersSchema);
+
+var schema = mongoose.Schema;
+var usersSchema = new schema({
+    Id: {type:Number, index:1},
+    FirstName: String,
+    LatName: String,
+    Email: String,
+    ProfilePic: String,
+    HomeTown: String
+},{collection: 'users'});
+
+var users = conn.model('UserM');
 
 // On connection error
 conn.on('error',function(err){
@@ -11,5 +25,6 @@ conn.on('error',function(err){
 // om connection success
 conn.once('open',function(){
 	console.log('connected.');
+
 	//mongoose.disconnect();
 });
