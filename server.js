fdd73a8a86c9ee7dece514abc5677869f3e57eb8
ONB_Benari <ONB_Benari@hotmail.com>
2015-06-09 11:14:24 +0300

    parser added

--- server.js
+++ server.js
@@ -1,9 +1,52 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 app.use('/',express.static('./public')).listen(process.env.PORT || 3000);
 console.log('listeing on server...');
 
 var addUser = require('./webservices/Database/users/db.user').addUser;
-addUser();
+
+// User Insert API
+app.post('/api/userInsert',function(req,res){
+	var lastId = 0;
+	lastId = addUser(req.body.user);
+
+	// Insert Circles.
+	// Insert User Circles
+
+	res.send("HERE " + lastId);
+});
+
+// User Friends Insert API
+app.post('/api/userFriendsInsert',function(req,res){
+
+	res.send("HERE ");
+});
+
+// Circle Insert API
+app.post('/api/circleInsert',function(req,res){
+	
+	res.send("HERE ");
+});
+
+// User Circle Insert API
+app.post('/api/userCircleInsert',function(req,res){
+
+	res.send("HERE ");
+});
+
+// User Category Friend Insert API
+app.post('/api/userCategoryFriendInsert',function(req,res){
+
+	res.send("HERE ");
+});
+
+// User Event Insert API
+app.post('/api/userEventInsert',function(req,res){
+
+	res.send("HERE ");
+});
 
--- webservices/Database/users/db.user.js
+++ webservices/Database/users/db.user.js
@@ -1,12 +1,13 @@
 /********       Connecting to database + Creating user schema            **************/
 
 /** **********************************************************/
-var addUser = function() {
+var addUser = function(userObj) {
     var mongoose = require('mongoose');
     mongoose.connect("mongodb://benari:123456@ds043972.mongolab.com:43972/db_suitemybeer");
     var userSchema = require('./db.user.schema').userSchema;
     var user = mongoose.model('UserM', userSchema);
     var conn = mongoose.connection;
+    var returnedValue = -1;
 
     conn.on('error', function (err) {
         console.log('Connection to the database as failed ' + err);
@@ -17,24 +18,33 @@ var addUser = function() {
 
         /**********       Adding new user from facebook to User's collection              **********/
         var newUser = new user({
-
-            FirstName: "FacebookFirstName",
-            LastName: "FacebookLastName",
-            Email: "FacebookUserEmail",
-            MediumProfilePic: "FacebookMediumProfilePic",
-            SmallProfilePic: "FacebookSmallProfilePic",
-            HomeTown: "FacebookUserHometown"
+            FirstName: userObj.first_name,
+            LatName: userObj.last_name,
+            Email: userObj.email,
+            MediumProfilePic: userObj.mediumProfilePicture,
+            SmallProfilePic: userObj.smallProfilePicture,
+            HomeTown: userObj.hometown.name,
+            FacebookId: userObj.id,
+            Gender: userObj.gender,
         });
+
+        //console.log('newUser',newUser);
 
         /**     Check if user already exist        **/
         if (newUser.isNew) {
             newUser.save(function (err, doc) {
                 console.log("\nUser was added to User collection " + doc);
+                // Get User Generated ID
+                returnedValue = newUser._id;
             })
         }
+
+        console.log(returnedValue);
     });
 
     mongoose.disconnect();
+    return returnedValue;
+
 };
 
 exports.addUser = addUser;
