4b9ff89a5f05e68f2682cee344c05e663cac44b9
Noam Rom <rom.noam89@gmail.com>
2015-06-11 11:51:56 +0300

    balls

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var app = express();
 
-app.use('/',express.static('./public')).listen(process.env.PORT || 3000);
+app.use('/',express.static('./public'),function(req,res,next){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+}).listen(process.env.PORT || 3000);
 console.log('listeing on server...');
 
 var addUser = require('./webservices/Database/users/db.user').addUser;
-addUser();
+//addUser();
 
