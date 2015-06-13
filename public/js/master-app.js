fbd5354cc367c9e96dbaa19e0d5603f053b943a5
ONB_Benari <ONB_Benari@hotmail.com>
2015-06-13 17:01:11 +0300

    jojso

--- public/js/master-app.js
+++ public/js/master-app.js
@@ -94,7 +94,10 @@ suiteApp.controller('masterCntrl', function($scope,$http,$location) {
               error(function(data, status, headers, config) {
                 // called asynchronously if an error occurs
                 // or server returns response with an error status.
+                console.log('Error : data', data);
                 console.log('Error : status', status);
+                console.log('Error : headers', headers);
+                console.log('Error : config', config);
                 // Redirect user back to login page
                 $location.path('signup');
               });       
