fdef57031893c7e67583f2c4ce7f604101ca4524
ONB_Benari <ONB_Benari@hotmail.com>
2015-06-13 14:50:55 +0300

    s

--- public/js/master-app.js
+++ public/js/master-app.js
@@ -87,7 +87,7 @@ suiteApp.controller('masterCntrl', function($scope,$http,$location) {
               error(function(data, status, headers, config) {
                 // called asynchronously if an error occurs
                 // or server returns response with an error status.
-                console.log('Error : status');
+                console.log('Error : status', status);
                 // Redirect user back to login page
                 $location.path('signup');
               });        
